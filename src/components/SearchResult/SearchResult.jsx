1461251e9869cc771fc1fd1a35e90d464fec52a8
unknown <jonathang@hotmail.com.ar>
2021-01-03 01:10:20 -0300

    Agregando funcionalidad al search

---
+++ src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import data from "../../data/productsNew";
+const SearchResult = ({ nombre }) => {
+  const [result, setResult] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  console.log(nombre);
+
+  const inputValues = (nombre) => {
+    if (!nombre.trim()) {
+      return setResult([]);
+    }
+    setInputValue(nombre);
+    resultValue();
+  };
+
+  useEffect(() => {
+    setInputValue(nombre);
+    resultValue();
+  }, [inputValue]);
+
+  const resultValue = () => {
+    const encontrado = data.map(
+      (e) => e.nombre.toLocaleLowerCase().indexOf(inputValue) !== -1 && e.nombre
+    );
+    setResult(encontrado);
+  };
+
+  return (
+    <div>
+      1s
+      <ul>
+        {result.map((e) => (
+          <p>{e}</p>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchResult;
